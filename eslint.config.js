3f3227315fe39d5605767b4de8854436e1dbd982
narghev <narghev@protonmail.com>
2025-09-29 12:34:18 +0400

    revert changes to eslint config, remove problematic dependencies from useEffect

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,4 @@ const raycastConfig = require("@raycast/eslint-config");
 
 module.exports = defineConfig([
   ...raycastConfig,
-  {
-    plugins: {
-      "react-hooks": require("eslint-plugin-react-hooks"),
-    },
-    rules: {
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-    },
-  },
 ]);
--- src/tune.tsx
+++ src/tune.tsx
@@ -12,7 +12,7 @@ export default function Command() {
     return () => {
       stopContinuousListening();
     };
-  }, [startContinuousListening, stopContinuousListening]);
+  }, []);
 
   if (error) {
     return (
